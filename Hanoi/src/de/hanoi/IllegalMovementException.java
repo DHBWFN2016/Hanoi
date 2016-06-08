09caa8756a9bb73cd71ee37afcaa5eef759c3e83
goellnerphillip <goellner.phill-tit15@it.dhbw-ravensburg.de>
2016-06-08 23:44:11 +0200

    Added Main class and argument pasing stump. Also some minor developement of the GamePad class

---
+++ Hanoi/src/de/hanoi/IllegalMovementException.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.hanoi;
+
+/**
+ * @author phillip.goellner
+ *
+ */
+public class IllegalMovementException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IllegalMovementException()
+	{
+		
+	}
+}
--- Hanoi/src/de/hanoi/Rod.java
+++ Hanoi/src/de/hanoi/Rod.java
@@ -12,4 +12,17 @@ import java.util.Stack;
 public class Rod
 {
 	private Stack<Disc> stack;
+	
+	public Disc pop()
+	{
+		return stack.pop();
+	}
+	public void push(Disc d)
+	{
+		stack.push(d);
+	}
+	public boolean isEmpty()
+	{
+		return stack.isEmpty();
+	}
 }
